149d1acf092ca6c6970ebc8acbccfcacb1547dc8
Nohac <jonas.rudlang@gmail.com>
2023-08-07 12:27:24 +0200

    build: add and track build directory

---
+++ build/nativeModule.d.ts
@@ -0,0 +1,13 @@
+import type { MSALResult, MSALInteractiveParams, MSALSilentParams, MSALSignoutParams, MSALAccount, MSALConfiguration } from './types';
+type RNMSALNativeModule = {
+    createPublicClientApplication(config: MSALConfiguration): Promise<void>;
+    acquireToken(params: MSALInteractiveParams): Promise<MSALResult | undefined>;
+    acquireTokenSilent(params: MSALSilentParams): Promise<MSALResult | undefined>;
+    getAccounts(): Promise<MSALAccount[]>;
+    getAccount(accountIdentifier: string): Promise<MSALAccount | undefined>;
+    removeAccount(account: MSALAccount): Promise<boolean>;
+    signout(params: MSALSignoutParams): Promise<boolean>;
+};
+declare const RNMSAL: RNMSALNativeModule;
+export default RNMSAL;
+//# sourceMappingURL=nativeModule.d.ts.map
